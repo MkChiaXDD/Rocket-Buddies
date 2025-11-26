561a069524429d31524fcebdf5f64d9cc94dafe5
MkChiaXDD <122813605+MkChiaXDD@users.noreply.github.com>
2025-11-26 17:05:45 +0800

    new grid for damage

---
+++ Assets/Scripts/Interactable/Damage.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    private HealthManager hpMgr;
+    private void Start()
+    {
+        hpMgr = FindFirstObjectByType<HealthManager>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            hpMgr.Damage(10);
+        }
+    }
+}
