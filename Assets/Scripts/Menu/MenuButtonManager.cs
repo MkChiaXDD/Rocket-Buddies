fb797d4a8a835a73103835a133c960166886ae4e
MkChiaXDD <122813605+MkChiaXDD@users.noreply.github.com>
2025-12-03 15:24:42 +0800

    Lil update

--- Assets/Scripts/Menu/MenuButtonManager.cs
+++ Assets/Scripts/Menu/MenuButtonManager.cs
@@ -97,6 +97,6 @@ public class MenuButtonManager : MonoBehaviour
 
     public void PlayButton()
     {
-        SceneManager.LoadScene("LevelOne");
+        SceneManager.LoadScene("WorldOne");
     }
 }
