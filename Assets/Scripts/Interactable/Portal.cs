326f686a8b85c25adfe02764b1ba954644490944
MkChiaXDD <122813605+MkChiaXDD@users.noreply.github.com>
2025-12-09 15:23:49 +0800

    Update
    Portal

--- Assets/Scripts/Interactable/Portal.cs
+++ Assets/Scripts/Interactable/Portal.cs
@@ -4,19 +4,28 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour
 {
     [SerializeField] private Portal linkedPortal;
+    [SerializeField] private bool IsExitPortal = false;
 
     private bool player1Reached = false;
     private bool player2Reached = false;
 
+    private GameObject player1;
+    private GameObject player2;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (linkedPortal == null) return;
+        if (IsExitPortal) return;
+
         if (collision.gameObject.name == "Player1")
         {
             player1Reached = true;
+            player1 = collision.gameObject;
         }
         else if (collision.gameObject.name == "Player2")
         {
             player2Reached = true;
+            player2 = collision.gameObject;
         }
         else
         {
@@ -25,7 +34,32 @@ public class Portal : MonoBehaviour
 
         if (player1Reached && player2Reached)
         {
-           
+            if (player1 && player2 && linkedPortal)
+            {
+                player1.transform.position = linkedPortal.transform.position;
+                player2.transform.position = linkedPortal.transform.position;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (linkedPortal == null) return;
+        if (IsExitPortal) return;
+
+        if (collision.gameObject.name == "Player1")
+        {
+            player1Reached = false;
+            player1 = null;
+        }
+        else if (collision.gameObject.name == "Player2")
+        {
+            player2Reached = false;
+            player2 = null;
+        }
+        else
+        {
+            return;
         }
     }
 }
