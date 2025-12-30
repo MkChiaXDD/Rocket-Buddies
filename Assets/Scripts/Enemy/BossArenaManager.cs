13f913eb2b68bfbd26f49cb98c384481f6f6485e
MkChiaXDD <mengkiatchia@gmail.com>
2025-12-30 23:25:13 +0800

    temporary

--- Assets/Scripts/Enemy/BossArenaManager.cs
+++ Assets/Scripts/Enemy/BossArenaManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class BossArenaManager : MonoBehaviour
 {
+    [Header("Chainsaw Attack")]
+    [SerializeField] private Transform chainsaw;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
