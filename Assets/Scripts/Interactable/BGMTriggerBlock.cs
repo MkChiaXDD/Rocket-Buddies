40d52982981e438361790699c33216bc46a1886c
MkChiaXDD <122813605+MkChiaXDD@users.noreply.github.com>
2026-02-04 15:48:12 +0800

    BGM playing thing

--- Assets/Scripts/Interactable/BGMTriggerBlock.cs
+++ Assets/Scripts/Interactable/BGMTriggerBlock.cs
@@ -4,11 +4,12 @@ public class BGMTriggerBlock : MonoBehaviour
 {
     [SerializeField] private string bgmName;
     [SerializeField] private bool loop = true;
+    [SerializeField] [Range(0, 1)] private float volume;
 
-    private void OnTriggerEnte2D(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
 
-        AudioManager.Instance.PlayBGM(bgmName, loop);
+        AudioManager.Instance.PlayBGM(bgmName, loop, volume);
     }
 }
